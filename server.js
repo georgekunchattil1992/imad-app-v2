27aea8be0a19019bdfc291694d69d3b58c7d7f2a
George Jacob <George Jacob>
2017-03-08 13:50:04 +0530

    htmlTemplate  updated in Article One.html

--- server.js
+++ server.js
@@ -20,6 +20,32 @@ var articleOne={
   			</p>`
 };
 
+var htmlTemplate=`
+<html>
+  <head>
+  	<title>
+  		${title}
+  	</title>
+  	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+  </head>
+  	<body>
+  		<div>
+  			<a href="/">Home</a>
+  		</div>
+  		<hr/>
+  		<h3>
+  			${heading}
+  		</h3>
+  		<div>
+  			${date}
+  		</div>
+  		<div>
+  			${content}
+  		</div>
+  	</body>
+</html>
+
+`;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
