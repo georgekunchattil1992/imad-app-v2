c1bfd71f6fa77ba6ddb0ff264682b3e33f4f9aad
George Jacob <George Jacob>
2017-03-17 22:48:00 +0530

    setInterval for animation

--- ui/main.js
+++ ui/main.js
@@ -8,5 +8,5 @@ element.innerHTML="New value";
 //Move the image
 var img=document.getElementById('madi');
 img.onclick=function(){
-	img.style.marginLeft='100px';
+	var interval=setInterval(moveRight,100);
 };
