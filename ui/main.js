4744bfe05a1b5b3e7899b8ea3a8e3b97965f2883
georgekunchattil1992 <georgekunchattil@gmail.com>
2017-03-22 23:02:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,7 @@ button.onclick=function(){
 var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
-submit.onclick=function();
+submit.onclick=function(){
   //Make a request to the server and send the name
   
   //Capture a list of names and render it as a list
@@ -39,3 +39,4 @@ submit.onclick=function();
   }
   var ul=document.getElementById('namelist');
   ul.innerHTML=list;
+};
