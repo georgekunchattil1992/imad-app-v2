2b9f02da75aca501e98e5b0d1cd5cfff5d571537
georgekunchattil1992 <georgekunchattil@gmail.com>
2017-03-24 12:05:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.get('/test-db',function(req,res){
 	//article(articleName={} content object for article-one
 	
 	// SELECT * FROM articles WHERE title='article-one'
-	pool.query("SELECT * FROM article WHERE title='" +req.params.articleName + "'", function(err,result){
+	pool.query("SELECT * FROM article WHERE title='" + req.params.articleName + "'", function(err,result){
 	    if(err){
 	        res.status(500).send(err.toString());  //Status error
 	    }
