a98c4484ecbe78b6b7ae40217d25b360b3b0ee33
georgekunchattil1992 <georgekunchattil@gmail.com>
2017-03-24 11:21:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -38,7 +38,31 @@ app.get('/test-db',function(req,res){
         }
     });
     
-});
+    
+ var counter=0;
+ app.get('/counter',function(req,res){
+  counter=counter+1;
+  res.send(counter.toString());
+ });
+
+ var names=[];
+ app.get('/submit-name',function(req,res){
+    //Get the name from the request
+    var name=req.query.name;
+    
+    names.push(name);
+    //JSON: Javascript Object Notation
+    res.send(JSON.stringify(names));
+  });
+
+  app.get('/:articleName', function(req,res){
+	//articleName==article-one
+	//article(articleName={} content object for article-one
+	var articleName=req.params.articleName;
+	res.send(createTemplate(articles[articleName]));
+   });
+
+ });
 
 app.listen(8080,function(){
 	console.log('app is listening on 8080 !');
