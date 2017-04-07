dc28316198e12591fa0fc976460cdb7a20bca945
georgekunchattil1992 <georgekunchattil@gmail.com>
2017-04-07 22:31:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,13 +128,17 @@ app.post('/login', function (req,res){
     var username=req.body.username;
     var password=req.body.password;
    
-
-   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err,result){
+   pool.query('SELECT * from "user" username= $1', [username], function (err,result){
        if(err){
            res.status(500).send(err.toString());
        }
        else{
-           res.send("User successfully created: " + username);
+           if(result.rows.length===0){
+               res.send(403).send('username/password is invalid'); //status 403--> forbidden
+           }
+           else{
+                 res.send("User successfully created: " + username);
+               }
        }
    });
 });
