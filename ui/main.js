5a5ee1263034baca2bef3cc0605f635cbd6d84f0
georgekunchattil1992 <georgekunchattil@gmail.com>
2017-04-08 11:37:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -159,6 +159,30 @@ app.post('/login', function (req,res){
   }); 
                  */
  
+ //Submit username/password to login
+ var submit=document.getElementById('submit_btn');
+ submit.onclick=function(){
+     // Create a request object
+     var request=new XMLHttpRequest();
+     
+     // Capture the response and store it in a variable
+     request.onreadystatechange=function(){
+         if(request.readyState===XMLHttpRequest.DONE){
+             //Take some action
+             if(request.status===200){
+                 // Capture a list of names and render it as a list
+                 var names=request.responseText;
+                 names=JSON.parse(names);
+                 var list='';
+                 for (var i=0;i<names.length;i++){
+                     list+='<li>'+names[i]+'</li>';
+                 }
+                 var yl=document.getElementById('namelist');
+                 ul.innerHTML=list
+             }
+         }
+     }
+ }
  
 
 //articles in dynamic format
@@ -188,7 +212,11 @@ app.post('/login', function (req,res){
 
  });
  
- 
+ //Make the request
+ var nameInput=document.getElementById('name');
+ var name=nameInput.value;
+ request.open('POST','http://georgekunchattil1992.imad.hasura-app.io/submit?name='+name, true);
+ request.send(JSON.stringify({username: username, password: password}));
 
 app.listen(8080,function(){
 	console.log('app is listening on 8080 !');
